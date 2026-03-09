f1e1d17545a1411312f502766e402d4a4de1ea7f
sanjay rao <sanjayraops17@gmail.com>
2026-03-09 20:42:37 +0530

    even words printer and case convert

---
+++ A4/src/main/java/a4/a4/A4.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package a4.a4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sanja
+ */
+public class A4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the sentence:\n");
+        String[] words = sc.nextLine().split("\\s+");
+        String toggle = "";
+        for (String word : words) {
+            if (word.length() % 2 == 0) {
+                String temp = "";
+                for (int i = 0; i < word.length(); i += 2) {
+                    temp = temp + word.charAt(i + 1) + word.charAt(i);
+                }
+                System.out.print(temp + " ");
+            }
+            for (char c : word.toCharArray()) {
+                if (Character.isUpperCase(c)) {
+                    toggle += Character.toLowerCase(c);
+                } else {
+                    toggle += Character.toUpperCase(c);
+                }
+            }
+            toggle += " ";
+        }
+        System.out.println("\nToggled sentence: \n" + toggle);
+    }
+
+}
